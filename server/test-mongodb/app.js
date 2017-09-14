754ae97e222fb07667ac3967a76e1382a48735f0
viveknair89 <viveknair89@ccs.neu.edu>
2017-09-14 06:33:13 +0530

    initial commit

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -9,7 +9,15 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
 
-  var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
+  // var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
+  var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+  if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds129434.mlab.com:29434/heroku_vlss0c70'; // connection string
+  }
+
 
   var mongoose = require("mongoose");
     mongoose.connect(connectionString);
