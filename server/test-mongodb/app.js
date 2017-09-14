54398304a4bf21224150cf84be3b3658396d3504
viveknair89 <viveknair89@ccs.neu.edu>
2017-09-15 03:17:16 +0530

    change connection

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -9,8 +9,8 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
 
-  // var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
-  var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+  var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
+  // var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
   if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
